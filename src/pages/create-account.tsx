32fc4702aafd3e077f3ce0d70bb9f4feb4fc643b
Christian Vallat <christianvallat02@gmail.com>
2024-01-07 12:41:57 -0600

    AWS changes for hosting

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -23,7 +23,7 @@ export default function CreateAccount(props: IPageProps) {
   const createAccount = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     fetch(
-      `https://livepartyhelper.com/Party/add-user?username=${createAccountInfo.username}&password=${createAccountInfo.password}&phone_number=${createAccountInfo.phoneNumber}`,
+      `http://partypush.us-east-1.elasticbeanstalk.com/Party/add-user?username=${createAccountInfo.username}&password=${createAccountInfo.password}&phone_number=${createAccountInfo.phoneNumber}`,
       {
         method: "GET",
         headers: {
